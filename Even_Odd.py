066069333ad4a213a5765c99893f1711ce2c151f
ankitgujre <ankitgujre374@gmail.com>
2026-02-06 10:45:33 +0530

    even odd function

---
+++ Even_Odd.py
@@ -0,0 +1,10 @@
+num = float(input("Enter Number: "))
+
+def check_Even_Odd(num):
+    if num % 2 == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
+        
+        
+check_Even_Odd(num)
