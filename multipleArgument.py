2533da511c6d0786b7ce1e139f04be8d33cbf1a7
ankitgujre <ankitgujre374@gmail.com>
2026-02-11 13:33:41 +0530

    two arguments

---
+++ multipleArgument.py
@@ -0,0 +1,9 @@
+def functionargs(c,d):
+    print("A = ", c)
+    print("B = ", d)
+    
+    
+    
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+functionargs(a,b)
