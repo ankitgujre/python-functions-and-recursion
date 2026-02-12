cfbffa5071ee55bcbed2730da40ac8103234ba3b
ankitgujre <ankitgujre374@gmail.com>
2026-02-13 00:02:17 +0530

    Recursion factorial

---
+++ RecursionFactorial.py
@@ -0,0 +1,10 @@
+# the function which call itself is called recursion
+
+def factorial(x):
+    if x == 1:
+        return 1
+    else:
+        return(x*factorial(x-1))
+
+num = int(input("Enter number: "))
+print("The factorial of ",num,"is ",factorial(num) )
